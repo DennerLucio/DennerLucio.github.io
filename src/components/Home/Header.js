008199a1e60fb34e34dd7592e014b488b6b8cd27
Denner Lucio <denner.luciosl@gmail.com>
2023-01-24 21:26:32 -0300

    atualização dos icones

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -20,9 +20,11 @@ const Header = () => {
       a = !a
       card.classList.toggle("flip");
     if(a){
-      bugIdiota.style.opacity = '1'}
+      bugIdiota.style.opacity = '1'
+      bugIdiota.style.zIndex = '1'}
     else{
-      bugIdiota.style.opacity = '0'}
+      bugIdiota.style.opacity = '0'
+      bugIdiota.style.zIndex = '-1'}
     });
 
 
@@ -30,9 +32,11 @@ const Header = () => {
       b = !b
       card1.classList.toggle("flip");
     if(b){
-      bugIdiota1.style.opacity = '1'}
+      bugIdiota1.style.opacity = '1'
+      bugIdiota1.style.zIndex = '1'}
     else{
-      bugIdiota1.style.opacity = '0'}
+      bugIdiota1.style.opacity = '0'
+      bugIdiota1.style.zIndex = '-1'}
     });
 
 
@@ -40,9 +44,11 @@ const Header = () => {
       c = !c
       card2.classList.toggle("flip");
     if(c){
-      bugIdiota2.style.opacity = '1'}
+      bugIdiota2.style.opacity = '1'
+      bugIdiota2.style.zIndex = '1'}
     else{
-      bugIdiota2.style.opacity = '0'}
+      bugIdiota2.style.opacity = '0'
+      bugIdiota2.style.zIndex = '-1'}
     });
 
 
@@ -50,9 +56,11 @@ const Header = () => {
       d = !d
       card3.classList.toggle("flip");
     if(d){
-      bugIdiota3.style.opacity = '1'}
+      bugIdiota3.style.opacity = '1'
+      bugIdiota3.style.zIndex = '1'}
     else{
-      bugIdiota3.style.opacity = '0'}
+      bugIdiota3.style.opacity = '1'
+      bugIdiota3.style.zIndex = '-1'}
     });
 
 
@@ -65,52 +73,57 @@ const Header = () => {
 
           <div className="flip" id="card">
             <div className="face" id="front">
-              <img className="icone" src="/icons/localizacao.png" alt="icone_localização" />
+              <img className="icone" src="/icons/local.png" alt="icone_localização" />
             </div>
             <div className="face" id="back">
               <div className="paredebranca">
-                <img className="icone_back" src="/icons/local_edit.png" alt="icone_teste" />
-              <p>Sarzedo - MG</p>
+              <a href="https://www.google.com/maps/place/Sarzedo,+MG,+32450-000/@-20.0362601,-44.1438109,15z/data=!4m6!3m5!1s0xa6c700b65494e5:0xb944a24b331c7114!8m2!3d-20.0359196!4d-44.1456593!16s%2Fg%2F11bxfycp72" target="_blank" rel="noopener noreferrer">
+                <img className="icone_back" src="/icons/localizacao.png" alt="icone_teste" />
+                </a>
+                <p className="p1">MG</p>
+                <p>Sarzedo</p>
               </div>
             </div>
           </div>
 
           <div className="flip" id="card1">
             <div className="face" id="front">
-              <img className="icone" src="/icons/localizacao.png" alt="icone_localização" />
+              <img className="icone" src="/icons/watsblack.png" alt="icone_localização" />
             </div>
             <div className="face" id="back1">
               <div className="paredebranca" >
-                <img className="icone_back" src="/icons/local_edit.png" alt="icone_teste" />
-                <p>Sarzedo - MG</p>
+                
+                <img className="icone_back" src="/icons/whatsapp.png" alt="icone_teste" />
+              
+                
+                <p className="p1">(31)99803-6573</p>
               </div>
             </div>
           </div>
 
           <div className="flip" id="card2">
             <div className="face" id="front">
-              <img className="icone" src="/icons/localizacao.png" alt="icone_localização" />
+              <img className="icone" src="/icons/github.png" alt="icone_localização" />
             </div>
             <div className="face" id="back2">
               <div className="paredebranca" >
-                <img className="icone_back" src="/icons/local_edit.png" alt="icone_teste" />
-                <p>Sarzedo - MG</p>
+                <img className="icone_back" src="/icons/githubback.png" alt="icone_teste" />
+                <a  className="p1" href="https://github.com/DennerLucio" target="_blank" rel="noopener noreferrer">Denner Lucio</a>
               </div>
             </div>
           </div>
 
           <div className="flip" id="card3">
             <div className="face" id="front">
-              <img className="icone" src="/icons/localizacao.png" alt="icone_localização" />
+              <img className="icone" src="/icons/email.png" alt="icone_localização" />
             </div>
             <div className="face" id="back3">
               <div className="paredebranca" >
-                <img className="icone_back" src="/icons/local_edit.png" alt="icone_teste" />
-                <p>Sarzedo - MG</p>
+                <img className="icone_back" src="/icons/gmail.png" alt="icone_teste" />
+                <p  className="p1">denner.luciosl@gmail.com</p>
               </div>
             </div>
           </div>
-
 
         </div>
       </div>
