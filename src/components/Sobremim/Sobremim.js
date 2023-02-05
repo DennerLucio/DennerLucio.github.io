0eb0918fa4d8cb4434a72e2b37e4cde72500ea69
Denner Lucio <denner.luciosl@gmail.com>
2023-02-04 22:38:38 -0300

    complemento de componente e ajustes nos demais layouts

--- src/components/Sobremim/Sobremim.js
+++ src/components/Sobremim/Sobremim.js
@@ -3,10 +3,16 @@ import "./Sobremim.css";
 const Sobremim = () => {
   return (
     <div className="sobremim">
-     <div id="tituloContato"><h1 id="TopicoContato"><a className="GradienteSubli" href="#al">Sobre Mim</a></h1></div>
-        <div>
-
-        </div>
+      <div id="tituloContato"><h1 id="TopicoContato"><a className="GradienteSubli" href="#al">Sobre</a></h1></div>
+      <div className="sobre_mim">
+          <div className="txt"> 
+          <p id="boas_vindas">Seja bem vindo a minha pagina!</p>
+          <p className="mar">Cursando Sistemas de Informação (Estácio).</p>        
+          <p className="mar">Técnico em Desenvolvimento de Sistemas.</p>      
+          <p>Atualmente atuo na área de desenvolvimento de testes automatizados para sistemas ERP na empresa NASAJON, também desenvolvo projetos (pessoais) front-end e back-end, 
+            experiência com metodologias ágeis (Scrum e Kanban), clean code.</p> </div>
+          <div className="foto_perfil"></div>
+      </div>
     </div>
   );
 };
